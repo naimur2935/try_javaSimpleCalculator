84ae3df130fc509dfab22c5b0a765ffae788abb4
Md Naimur Rahman <naimur2935@gmail.com>
2024-12-17 13:14:35 +0600

    Update SimpleCalculatorController.java

--- src/main/java/bd/edu/seu/calculator/SimpleCalculatorController.java
+++ src/main/java/bd/edu/seu/calculator/SimpleCalculatorController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class SimpleCalculatorController {
     @FXML
     private TextField display; // Main display for inputs and results
@@ -18,21 +21,31 @@ public class SimpleCalculatorController {
 
     private double results = 0.0;
 
+    private String bracketOparetions = "";
+    private int firstBracket = 0;
+
     private void displayHistory() {
         textLabel.setText(history);
     }
 
     // Displays the input numbers
     private void displayNumbers(String number) {
-        if (currentOperator.isEmpty()) {
-            firstNumber += number;
+        if (!bracketOparetions.isEmpty()) {
+            bracketOparetions += number;
             history += number;
+            display.setText(number);
+            displayHistory();
         } else {
-            secondNumber += number;
-            history += number;
+            if (currentOperator.isEmpty()) {
+                firstNumber += number;
+                history += number;
+            } else {
+                secondNumber += number;
+                history += number;
+            }
+            display.setText(currentOperator.isEmpty() ? firstNumber : secondNumber);
+            displayHistory();
         }
-        display.setText(currentOperator.isEmpty() ? firstNumber : secondNumber);
-        displayHistory();
     }
 
     public void button7Action(ActionEvent actionEvent) {
@@ -48,14 +61,20 @@ public class SimpleCalculatorController {
     }
 
     public void buttonDivAction(ActionEvent actionEvent) {
-        if (currentOperator.isEmpty())
-            currentOperator = "/";
-        else {
-            calculation();
-            currentOperator = "/";
+        if (firstBracket > 0) {
+            bracketOparetions += "/";
+            history += "/";
+            displayHistory();
+        } else {
+            if (currentOperator.isEmpty())
+                currentOperator = "/";
+            else {
+                calculation();
+                currentOperator = "/";
+            }
+            history += currentOperator;
+            displayHistory();
         }
-        history += currentOperator;
-        displayHistory();
     }
 
     public void buttonBackAction(ActionEvent actionEvent) {
@@ -68,6 +87,7 @@ public class SimpleCalculatorController {
             firstNumber = firstNumber.substring(0, firstNumber.length() - 1);
         }
         history = history.substring(0, history.length() - 1);
+        bracketOparetions = bracketOparetions.substring(0, bracketOparetions.length() - 1);
         display.setText(currentOperator.isEmpty() ? firstNumber : secondNumber);
         displayHistory();
     }
@@ -80,6 +100,7 @@ public class SimpleCalculatorController {
         display.setText("");
         textLabel.setText("");
         results = 0.0;
+        bracketOparetions = "";
     }
 
     public void button4Action(ActionEvent actionEvent) {
@@ -95,6 +116,11 @@ public class SimpleCalculatorController {
     }
 
     public void buttonMulAction(ActionEvent actionEvent) {
+        if(firstBracket > 0){
+            bracketOparetions += "x";
+            history += "x";
+            displayHistory();
+        }else {
         if (currentOperator.isEmpty())
             currentOperator = "x";
         else {
@@ -103,14 +129,30 @@ public class SimpleCalculatorController {
         }
         history += currentOperator;
         displayHistory();
+        }
     }
 
     public void buttonOpenBracketAction(ActionEvent actionEvent) {
-//        this button action is "("
+        bracketOparetions += "(";
+        firstBracket++;
+        history += "(";
+        displayHistory();
     }
 
     public void buttonCloseBracketAction(ActionEvent actionEvent) {
-        //        this button action is ")"
+        if (firstBracket > 0) {
+            bracketOparetions += ")";
+            firstBracket--;
+            history += ")";
+            displayHistory();
+            if(firstBracket == 0){
+                double infixResult = inFixCalculate(bracketOparetions);
+                double initialResult = infixResult * Double.parseDouble(secondNumber);
+                secondNumber = String.valueOf(initialResult) ;
+              bracketOparetions = "";
+                calculation();
+            }
+        }
     }
 
     public void button1Action(ActionEvent actionEvent) {
@@ -126,14 +168,20 @@ public class SimpleCalculatorController {
     }
 
     public void buttonMinusAction(ActionEvent actionEvent) {
-        if (currentOperator.isEmpty())
-            currentOperator = "-";
-        else {
-            calculation();
-            currentOperator = "-";
+        if (firstBracket > 0) {
+            bracketOparetions += "-";
+            history += "-";
+            displayHistory();
+        } else {
+            if (currentOperator.isEmpty())
+                currentOperator = "-";
+            else {
+                calculation();
+                currentOperator = "-";
+            }
+            history += currentOperator;
+            displayHistory();
         }
-        history += currentOperator;
-        displayHistory();
     }
 
     public void buttonXSquareAction(ActionEvent actionEvent) {
@@ -171,53 +219,114 @@ public class SimpleCalculatorController {
     }
 
     public void buttonPlusAction(ActionEvent actionEvent) {
-        if (currentOperator.isEmpty())
-            currentOperator = "+";
-        else {
-            calculation();
-            currentOperator = "+";
+        if(firstBracket > 0){
+            bracketOparetions += "+";
+            history += "+";
+            displayHistory();
+        }else {
+            if (currentOperator.isEmpty())
+                currentOperator = "+";
+            else {
+                calculation();
+                currentOperator = "+";
+            }
+            history += currentOperator;
+            displayHistory();
         }
-        history += currentOperator;
-        displayHistory();
-
     }
 
     public void buttonResultAction(ActionEvent actionEvent) {
         calculation();
     }
 
-    private void calculation(){
+    private void calculation() {
         // int or double?
 
         double fNum = Double.parseDouble(firstNumber);
         double sNum = Double.parseDouble(secondNumber);
 
-        switch (currentOperator){
-            case "+" ->{
-                results = fNum+sNum;
+        switch (currentOperator) {
+            case "+" -> {
+                results = fNum + sNum;
                 display.setText(String.valueOf(results));
             }
             case "-" -> {
-                results = fNum-sNum;
+                results = fNum - sNum;
                 display.setText(String.valueOf(results));
             }
 
-            case "x" ->{
-                results = fNum*sNum;
+            case "x" -> {
+                results = fNum * sNum;
                 display.setText(String.valueOf(results));
             }
 
             case "/" -> {
-                results = fNum/sNum;
+                results = fNum / sNum;
                 display.setText(String.valueOf(results));
             }
 
             case "%" -> {
-                results = (fNum/100)*sNum;
+                results = (fNum / 100) * sNum;
                 display.setText(String.valueOf(results));
             }
         }
         firstNumber = String.valueOf(results);
         secondNumber = "";
     }
+
+
+
+    public static double inFixCalculate(String bracketOparetionString) {
+        Stack<Double> numStack = new Stack<>();
+        Stack<Character> opStack = new Stack<>();
+
+        for (int i = 0; i < bracketOparetionString.length(); i++) {
+            char current = bracketOparetionString.charAt(i);
+
+            if (Character.isDigit(current)) {
+                StringBuilder number = new StringBuilder();
+                while (i < bracketOparetionString.length() && (Character.isDigit(bracketOparetionString.charAt(i)) || bracketOparetionString.charAt(i) == '.')) {
+                    number.append(bracketOparetionString.charAt(i));
+                    i++;
+                }
+                i--;
+                numStack.push(Double.parseDouble(number.toString()));
+            } else if (current == '(') {
+                opStack.push(current);
+            } else if (current == ')') {
+                while (!opStack.isEmpty() && opStack.peek() != '(') {
+                    numStack.push(apply(opStack.pop(), numStack.pop(), numStack.pop()));
+                }
+                opStack.pop();
+            } else if (current == '+' || current == '-' || current == '*' || current == '/') {
+                while (!opStack.isEmpty() && priority(current) <= priority(opStack.peek())) {
+                    numStack.push(apply(opStack.pop(), numStack.pop(), numStack.pop()));
+                }
+                opStack.push(current);
+            }
+        }
+
+        while (!opStack.isEmpty()) {
+            numStack.push(apply(opStack.pop(), numStack.pop(), numStack.pop()));
+        }
+
+        return numStack.pop();
+    }
+
+    public static double apply(char op, double b, double a) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+            default: throw new RuntimeException("Invalid operator");
+        }
+    }
+
+    public static int priority(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return -1;
+    }
+
 }
